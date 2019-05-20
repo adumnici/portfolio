b25e6de2189254bea6603a28a880e30de5547893
AdrianMAD <46680955+AdrianMAD@users.noreply.github.com>
2019-05-20 14:29:41 +0300

    small changes

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ function dropdown () {
         doc.className = 'topnav';
     } 
 }    
+
+// Go to top
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
